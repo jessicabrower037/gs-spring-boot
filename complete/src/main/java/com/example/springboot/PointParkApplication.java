9cf08e4756a7460a7a00f0f2eeb4e2ee8de1199f
jessicabrower037 <121197501+jessicabrower037@users.noreply.github.com>
2024-02-01 13:23:20 -0500

    Create PointParkApplication.java

---
+++ complete/src/main/java/com/example/springboot/PointParkApplication.java
@@ -0,0 +1,15 @@
+package com.example.springboot;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class PointParkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
